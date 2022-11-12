38b869a724a502e88e053bc14887d400efb6ca10
Joel <joel.varghese2020@vitbhopal.ac.in>
2022-11-12 23:39:03 +0530

    starting files and model added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainScreen from './Components/screens/login_sign_up/MainScreen';
 
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-       
+    <Route path="/" element={<MainScreen/>}/>
     </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Components/screens/login_sign_up/MainScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box, Container } from '@chakra-ui/react'
+const MainScreen = () => {
+  return (
+    <div>
+        <Container maxW='2xl' bg='blue.600' centerContent>
+        <Box>
+          asdas
+        </Box>
+</Container>
+    </div>
+  )
+}
+
+export default MainScreen
